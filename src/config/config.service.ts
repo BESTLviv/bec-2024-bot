6a6eda1f8fdcc3b26047d019421b2800c8861dd9
Malerce933 <brazdik14@gmail.com>
2024-09-07 01:21:41 +0300

    fix

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,23 +1,39 @@
-import { config, DotenvParseOutput } from "dotenv";
+// import { config, DotenvParseOutput } from "dotenv";
+// import { IConfigService } from "./config.interface";
+
+// export class ConfigService implements IConfigService {
+//     private config: DotenvParseOutput;
+//     constructor() {
+//         const { error, parsed} = config();
+//         if(error) {
+//             throw new Error("Don`t find file .env")
+//         }
+//         if(!parsed) {
+//             throw new Error("Empty file .env")
+//         }
+//         this.config = parsed;
+//     }
+
+//     get(key: string): string {
+//         const res = this.config[key];
+//         if(!res) {
+//             throw new Error("There is no such key")
+//         }
+//         return res;
+//     }
+// }
+
 import { IConfigService } from "./config.interface";
 
 export class ConfigService implements IConfigService {
-    private config: DotenvParseOutput;
     constructor() {
-        const { error, parsed} = config();
-        if(error) {
-            throw new Error("Don`t find file .env")
-        }
-        if(!parsed) {
-            throw new Error("Empty file .env")
-        }
-        this.config = parsed;
+        // Не потрібно зчитувати файл .env, якщо використовуємо змінні середовища Heroku
     }
 
     get(key: string): string {
-        const res = this.config[key];
-        if(!res) {
-            throw new Error("There is no such key")
+        const res = process.env[key];
+        if (!res) {
+            throw new Error(`There is no such key: ${key}`);
         }
         return res;
     }
