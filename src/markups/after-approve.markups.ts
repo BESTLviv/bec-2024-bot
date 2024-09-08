f6496f8dc2042621f6d24ce56304fe32a1b12cfe
Malerce933 <brazdik14@gmail.com>
2024-09-08 13:16:30 +0300

    delete some menu

--- src/markups/after-approve.markups.ts
+++ src/markups/after-approve.markups.ts
@@ -1,6 +1,6 @@
 import { Markup } from "telegraf";
 
-export const menuOptionAfterApprove = ['Вакансії', 'Більше інформації про BEC', "Чат для учасників івену", "Загальна інформація для учасників", 'Моя команда']
+export const menuOptionAfterApprove = ['Вакансії', 'Більше інформації про BEC', "Чат для учасників івену", 'Моя команда']
 export const menuKeyboardAfterApprove = Markup.keyboard([
     [  
         Markup.button.callback(menuOptionAfterApprove[0], "menu_vacancies"),
@@ -8,7 +8,6 @@ export const menuKeyboardAfterApprove = Markup.keyboard([
     ],
     [  
         Markup.button.callback(menuOptionAfterApprove[2], "menu_chat"),
-        Markup.button.callback(menuOptionAfterApprove[3], "menu_surviv"),
     ],
     [  
         Markup.button.callback(menuOptionAfterApprove[4], "menu_my-team"),
--- src/markups/competition.markups.ts
+++ src/markups/competition.markups.ts
@@ -1,6 +1,6 @@
 import { Markup } from "telegraf";
 
-export const menuOptionCompetition = ['Вакансії', 'Більше інформації про BEC', "Чат для учасників івену", "Загальна інформація для учасників", "Розклад", 'Моя команда']
+export const menuOptionCompetition = ['Вакансії', 'Більше інформації про BEC', "Чат для учасників івену", 'Моя команда']
 export const menuKeyboardCompetition = Markup.keyboard([
     [  
         Markup.button.callback(menuOptionCompetition[0], "menu_vacancies"),
@@ -8,23 +8,20 @@ export const menuKeyboardCompetition = Markup.keyboard([
     ],
     [  
         Markup.button.callback(menuOptionCompetition[2], "menu_chat"),
-        Markup.button.callback(menuOptionCompetition[3], "menu_surviv"),
     ],
     [  
-        Markup.button.callback(menuOptionCompetition[4], "menu_schedule"),
         Markup.button.callback(menuOptionCompetition[5], "menu_my-team"),
     ],
 
 ]).resize();
 
-export const teamCompetitionOption = ["Назад", "Додати своє CV", "Основне завдання",  "Команді потрібна допомога"]
+export const teamCompetitionOption = ["Назад", "Додати своє CV", "Команді потрібна допомога"]
 export const teamCompetitionboard = Markup.keyboard([
     [  
         Markup.button.callback(teamCompetitionOption[0], "team-profile_back"),
         Markup.button.callback(teamCompetitionOption[1], "team-profile_add-repo"),
     ],
     [
-        Markup.button.callback(teamCompetitionOption[2], "team-profile_change-technologyList"),
         Markup.button.callback(teamCompetitionOption[3], "team-profile_add-cv"),
     ],
 
